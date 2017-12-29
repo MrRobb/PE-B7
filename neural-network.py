3d629cf139a2de8cba7758b4512a943c6eca76b4
Mr.Robb <roberto.ariosa@est.fib.upc.edu>
2017-12-30 00:00:22 +0100

    1 layer

--- neural-network.py
+++ neural-network.py
@@ -1,48 +1,98 @@
-# Importar MNIST data
-import input_data
-mnist = input_data.read_data_sets("/tmp/data/", one_hot=True)
-import tensorflow
+import math
+import tensorflow as tf
+from tensorflow.contrib.learn.python.learn.datasets.mnist import read_data_sets
 
-# Hyper parámetros
-learning_rate = 0.01
-training_iteration = 30
+
+# Parametros constantes
+iterations = 5000
+display_step = 5
+batch = 100
+tf.set_random_seed(0)
+sd = 0.1
+img_size = 28
+classification = 10
 batch_size = 100
-display_step = 2
 
 
-
-# Input
-## Una imagen de la base de datos es de tamaño 28x28=784
-x = tensorflow.placeholder("float", [None, 784])
-
-# Output
-## 0-9 dígitos => 10 clases
-y = tensorflow.placeholder("float", [None, 10])
+# Datos (input)
+## mnist.test (10.000 imagenes + labels) y mnist.train (60.000 imagenes + labels)
+mnist = read_data_sets("data", one_hot=True, reshape=False, validation_size=0)
 
 
+# Grafo
 
-# Crear modelo
+# Probability of keeping a node during dropout = 1.0 at test time (no dropout) and 0.75 at training time
+pkeep = tf.placeholder(tf.float32)
 
-# Poner weights del model
-## W = weights
-W = tensorflow.Variable(tensorflow.zeros([784, 10]))
-## b = biases
-b = tensorflow.Variable(tensorflow.zeros([10]))
+## [INPUT] 28 x 28 imagenes con un batch size "None" indeterminado
+X = tf.placeholder(tf.float32, [None, img_size, img_size, 1])
 
-with tensorflow.name_scope("Wx_b") as scope:
-    model = tensorflow.nn.softmax(tensorflow.matmul(x, W) + b)
+# Hidden layers
+K = 8       # convolutional
+W1 = tf.Variable(tf.truncated_normal([5, 5, 1, K], stddev=sd))
+B1 = tf.Variable(tf.truncated_normal([K], stddev=sd))
 
-# Añadir summary operaciones para obtener los datos
-w_h = tensorflow.histogram_summary("weigths", W)
-b_h = tensorflow.histogram_summary("biases", b)
+N = 200     # fully connected
+W2 = tf.Variable(tf.truncated_normal([7 * 7 * K, N], stddev=sd))
+B2 = tf.Variable(tf.truncated_normal([N], stddev=sd))
+            # output layer
+W3 = tf.Variable(tf.truncated_normal([N, classification], stddev=sd))
+B3 = tf.Variable(tf.truncated_normal([classification], stddev=sd))
 
-with tensorflow.name_scope("cost_function") as scope:
-    cost_function = -tensorflow.reduce_sum(y * tensorflow.log(model))
-    tensorflow.scalar_summary("cost_function", cost_function)
+## [OUTPUT] 0,1,2...9 digitos, aqui iran las respuestas
+Y_ = tf.placeholder(tf.float32, [None, classification])
 
-with tensorflow.name_scope("train") as scope:
-    optimizer = tf.train.GradientDescentOptimizer(learning_rate).minimize(cost_function)
 
-init = tf.initialize_all_variables()
+# Modelo
+stride = 4  # 28x28 --> 7x7
+Y1 = tf.nn.relu(tf.nn.conv2d(X, W1, strides=[1, stride, stride, 1], padding='SAME') + B1)
+Y_reshape = tf.reshape(Y1, shape=[-1, 7 * 7 * K])   # necesario para pasar de conv --> fully connected
+Y2 = tf.nn.relu(tf.matmul(Y_reshape, W2) + B2)
+Y_intermediate = tf.matmul(Y2, W3) + B3             # necesario para cross entropy
+Y = tf.nn.softmax(Y_intermediate)
 
-merged_summary_op = tf.merge_all_summaries()
+
+# Error
+cross_entropy = tf.nn.softmax_cross_entropy_with_logits(logits=Y_intermediate, labels=Y_)
+cross_entropy = tf.reduce_mean(cross_entropy)*batch_size
+
+# Calcular precision (0..1)
+correct_prediction = tf.equal(tf.argmax(Y, 1), tf.argmax(Y_, 1))
+accuracy = tf.reduce_mean(tf.cast(correct_prediction, tf.float32))
+
+# Entrenamiento
+lr = tf.placeholder(tf.float32)
+train_step = tf.train.AdamOptimizer(lr).minimize(cross_entropy)
+
+
+# Init
+init = tf.global_variables_initializer()
+
+train_loss = list()
+train_acc = list()
+test_loss = list()
+test_acc = list()
+
+# Training loop
+with tf.Session() as sess:
+    sess.run(init)
+
+    for i in range(iterations + 1):
+        batch_X, batch_Y = mnist.train.next_batch(batch_size)
+
+        max_learning_rate = 0.003
+        min_learning_rate = 0.0001
+        decay_speed = 2000.0
+        learning_rate = min_learning_rate + (max_learning_rate - min_learning_rate) * math.exp(-i/decay_speed)
+
+        if i % display_step == 0:
+            # training
+            train_acc, train_loss = sess.run([accuracy, cross_entropy], feed_dict={X: batch_X, Y_: batch_Y, pkeep: 1.0})
+
+            # testing
+            test_acc, test_loss = sess.run([accuracy, cross_entropy], feed_dict={X: mnist.test.images, Y_:mnist.test.labels, pkeep: 1.0})
+
+            # print
+            print("{}\t{}\t{}\t{}\t{}".format(i,train_acc,train_loss,test_acc,test_loss))
+
+        sess.run(train_step, feed_dict={X: batch_X, Y_: batch_Y, lr: learning_rate, pkeep: 0.75})
