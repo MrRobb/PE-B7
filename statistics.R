45e6971f67f4d7921d44d14c3377ab1576bd93ac
Mr.Robb <roberto.ariosa@est.fib.upc.edu>
2018-01-01 21:18:39 +0100

    png

--- statistics.R
+++ statistics.R
@@ -8,12 +8,19 @@ layer5data <- read.csv("results/5layer/5layer_5000iterations_1step.csv")
 
 # Plot data
 
-# png(file = "layer1data.png")
+png(file = "resultslayersdata.png")
 plot(layer1data$Test.Accuracy, type = "l", col = "red", xlab = "Iteraciones", ylab = "Probabilidad de acierto", 
-     main = "Precision del test", pch = 0:2000)
+     main = "Precision del test")
 lines(layer3data$Test.Accuracy, type = "l", col = "blue")
 lines(layer5data$Test.Accuracy, type = "l", col = "green")
-# dev.off()
+dev.off()
+
+png(file = "results/layersdatazoom.png")
+plot(layer1data$Test.Accuracy, type = "l", col = "red", xlab = "Iteraciones", ylab = "Probabilidad de acierto", 
+     main = "Precision del test", xlim = c(4900, 5000), ylim = c(0.98, 1.0))
+lines(layer3data$Test.Accuracy, type = "l", col = "blue")
+lines(layer5data$Test.Accuracy, type = "l", col = "green")
+dev.off()
 
 "layer1data"
 summary(layer1data$Iterations)
