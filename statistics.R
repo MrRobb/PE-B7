b6bb19c8ba5947a0a535d84c91b500b7d4a5df93
Mr.Robb <roberto.ariosa@est.fib.upc.edu>
2018-01-05 14:01:23 +0100

    r

--- statistics.R
+++ statistics.R
@@ -9,15 +9,22 @@ layer5data <- read.csv("results/5layer/5layer_5000iterations_1step.csv")
 # Plot data
 
 png(file = "resultslayersdata.png")
-plot(layer1data$Test.Accuracy, type = "l", col = "red", xlab = "Iteraciones", ylab = "Probabilidad de acierto", 
+plot(layer1data$Test.Accuracy, 
+     type = "l", col = "red", 
+     xlab = "Iteraciones", 
+     ylab = "Probabilidad de acierto", 
      main = "Precision del test")
 lines(layer3data$Test.Accuracy, type = "l", col = "blue")
 lines(layer5data$Test.Accuracy, type = "l", col = "green")
 dev.off()
 
 png(file = "results/layersdatazoom.png")
-plot(layer1data$Test.Accuracy, type = "l", col = "red", xlab = "Iteraciones", ylab = "Probabilidad de acierto", 
-     main = "Precision del test", xlim = c(4900, 5000), ylim = c(0.98, 1.0))
+plot(layer1data$Test.Accuracy, 
+     type = "l", col = "red", 
+     xlab = "Iteraciones", 
+     ylab = "Probabilidad de acierto", 
+     main = "Precision del test", 
+     xlim = c(4900, 5000), ylim = c(0.98, 1.0))
 lines(layer3data$Test.Accuracy, type = "l", col = "blue")
 lines(layer5data$Test.Accuracy, type = "l", col = "green")
 dev.off()
@@ -43,9 +50,9 @@ summary(layer5data$Training.Loss)
 summary(layer5data$Test.Accuracy)
 summary(layer5data$Test.Loss)
 
-n1 = length(layer1data$Training.Accuracy); n1
-n3 = length(layer3data$Training.Accuracy); n3
-n5 = length(layer5data$Training.Accuracy); n5
+n1 = length(layer1data$Training.Accuracy);
+n3 = length(layer3data$Training.Accuracy);
+n5 = length(layer5data$Training.Accuracy);
 
 s1 = sd (layer1data$Training.Accuracy)
 s3 = sd (layer3data$Training.Accuracy)
@@ -64,10 +71,10 @@ zalfa = qnorm(confi, mean = 0, sd = 1)
 if (zalfa < abs(z13))
 {
   cat("Rechazamos Ho de 1 y 3 -> 3 es mejor que 1, con una confianza del", confi*100, "%")
-} else  print("No podemos rechazar Ho de 1 y 3")   
+} else print("No podemos rechazar Ho de 1 y 3")   
 
 
 if (zalfa < abs(z35))
 {
   cat("Rechazamos Ho de 3 y 5 -> 5 es mejor que 3 con una confianza del", confi*100,"%")
-} else print("No podemos rechazar Ho de 3 y 5")   
+} else print("No podemos rechazar Ho de 3 y 5")
